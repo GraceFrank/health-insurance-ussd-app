7f7bedeace3a56d6598c1427cf07f998d74ecdc5
Grace Frank <frankgracey@gmail.com>
2019-10-01 06:24:30 +0100

    updated

--- app.js
+++ app.js
@@ -18,33 +18,11 @@ app.get('*', (req, res) => {
 
 app.post('*', (req, res) => {
   let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  if (text == '') {
-    transactions.start(res);
-  } else if (text == '1') {
-    // Business logic for first level response
-    let response = `CON Choose account information you want to view
-    1. Account number
-    2. Account balance`;
-    res.send(response);
-  } else if (text == '2') {
-    // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`;
-    res.send(response);
-  } else if (text == '1*1') {
-    // Business logic for first level response
-    let accountNumber = 'ACC1001';
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`;
-    res.send(response);
-  } else if (text == '1*2') {
-    // This is a second level response where the user selected 1 in the first instance
-    let balance = 'NGN 10,000';
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your balance is ${balance}`;
-    res.send(response);
-  } else {
-    res.status(400).send('Bad request!');
-  }
+  if (text == '') transactions.start(res);
+
+  transactions[text](res);
+
+  if (!transaction[text]) res.status(400).send('Bad request!');
 });
 
 const port = process.env.PORT || 3003;

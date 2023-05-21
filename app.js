0129777f00f41b8d7d7798b7eb1d3f3a993c777f
Grace <frankgracey@gmail.com>
2023-05-21 11:19:57 +0100

    updated packages on usd app and removed unused dependency

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const log = require('morgan');
-const connectToDb = require('./connect-database');
-const transactions = require('./transactions');
-const faker = require('faker');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const log = require("morgan");
+const connectToDb = require("./connect-database");
+const transactions = require("./transactions");
 
 const app = express();
 
-app.use(log('dev'));
+app.use(log("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   return res.send(
     `This is tutorial App on creating your first USSD app in 5 minutes or less by Ajala Abdulsamii <kgasta@gmail.com>`
   );
 });
 
-app.post('*', (req, res) => {
+app.post("*", (req, res) => {
   let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  if (text == '') transactions.start(res);
+  if (text == "") transactions.start(res);
 
   // if (!transaction[text] && text !== '') {
   //   let name = text.split('*');
--- connect-database.js
+++ connect-database.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 function connectToDb() {
   //get db from config module, depending on the node environment
   const db = process.env.DATABASE_URL;
@@ -7,12 +7,11 @@ function connectToDb() {
   return mongoose
     .connect(db, {
       useNewUrlParser: true,
-      useCreateIndex: true
     })
     .then(() => {
       console.log(`connected to database`);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       process.exit(1);
     });
